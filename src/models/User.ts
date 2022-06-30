8013f294fce9f2e378b12dda137bb4219a90a24f
Ayaf-Bless <blessambel1@gmail.com>
2022-06-30 15:13:02 +0300

    create the on method and the event store

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,14 +2,19 @@ interface UserProps {
   name?: string;
   age?: number;
 }
+type Callback = () => {};
 
 export class User {
+  events: { [key: string]: Callback } = {};
   constructor(private data: UserProps) {}
   get(propName: string): number | string {
     return this.data[propName];
   }
 
   set(update: UserProps): void {
+    // @ts-ignore
     Object.assign(this.data, update);
   }
+
+  on(eventName: string, callback: Callback) {}
 }
