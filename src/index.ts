d96563ff12047cb1d0dbba87a163b0e0ff1a62b3
Ayaf-Bless <blessambel1@gmail.com>
2022-07-12 20:31:17 +0300

    create the save method and it checks based on the id property to see if it should performe a POST or PUT request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { User } from "./models/User";
 
 const user = new User({ id: 1 });
+user.set({ name: "monica" });
+user.save();
+
+const user2 = new User({ name: "ross" });
+user2.save();
 
 user.fetch();
