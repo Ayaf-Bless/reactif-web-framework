c1da815d228bce4397629894168cba58265c0782
Ayaf-Bless <blessambel1@gmail.com>
2022-07-16 05:42:15 +0300

    create the Sync file that handles all the sync functions

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,13 @@
-import axios, { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
 
-interface UserProps {
+export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
 }
 
 export class User {
-  events: Eventing = new Eventing();
+  public events: Eventing = new Eventing();
   constructor(private data: UserProps) {}
   get(propName: string): number | string {
     return this.data[propName];
@@ -17,22 +16,5 @@ export class User {
   set(update: UserProps): void {
     // @ts-ignore
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get("id")}`)
-      .then((res: AxiosResponse): void => {
-        this.set(res.data);
-      });
-  }
-  save(): void {
-    const id = this.get("id");
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post(`http://localhost:3000/users/`, this.data);
-    }
   }
 }
