928021a3ee7d4bfa8c281a25f3df8011379fe492
Ayaf-Bless <blessambel1@gmail.com>
2022-07-16 06:10:39 +0300

    make the Sync file generic and handle the ID property

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,18 @@
 import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
 
 export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
 }
+const rootUrl = "http://localhost/users";
 
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
   constructor(private data: UserProps) {}
+
   get(propName: string): number | string {
     return this.data[propName];
   }
